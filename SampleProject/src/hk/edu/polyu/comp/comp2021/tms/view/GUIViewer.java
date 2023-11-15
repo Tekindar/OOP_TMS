4037cac2e73417f4c43552be805c7f9023276df1
Tekindar <116880404+Tekindar@users.noreply.github.com>
2023-11-15 16:21:40 +0800

    3

---
+++ SampleProject/src/hk/edu/polyu/comp/comp2021/tms/view/GUIViewer.java
@@ -0,0 +1,11 @@
+package hk.edu.polyu.comp.comp2021.tms.view;
+import java.awt.*;
+import javax.swing.*;
+
+public class GUIViewer {
+    JFrame f;
+
+    GUIViewer(){
+        f = new JFrame();
+    }
+}

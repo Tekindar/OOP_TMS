1058e486435ba90095c6f71f34e6c2aec44312ee
Tekindar <116880404+Tekindar@users.noreply.github.com>
2023-11-15 22:15:36 +0800

    gui2

--- SampleProject/src/hk/edu/polyu/comp/comp2021/tms/view/GUIViewer.java
+++ SampleProject/src/hk/edu/polyu/comp/comp2021/tms/view/GUIViewer.java
@@ -7,14 +7,22 @@ import javax.swing.*;
 
 public class GUIViewer {
     JFrame f;
-    Font UniFont;
+    Font UniFont(int size){
+        return new Font("LEMON", Font.BOLD, size);
+    }
+    Font UniFont(){
+        return new Font("LEMON", Font.BOLD, 30);
+    }
 
     void mainFrame(){
+        // Components
         JButton quit = new JButton("Quit");
-        quit.setFont(UniFont);
+        JButton CST = new JButton("CreateSimpleTask");
 
         //properties
+        quit.setFont(UniFont());
         quit.setSize(100,100);
+        quit.setLocation(350,350);
         quit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -22,17 +30,43 @@ public class GUIViewer {
                 System.exit(0);
             }
         });
+        CST.setFont(UniFont(20));
+        CST.setSize(600,100);
+        CST.setLocation(100,50);
+        CST.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                f.setVisible(false);
+                CreateSimpleTask();
+            }
+        });
 
         //layout
+        f.setSize(800,800);
+        f.setLayout(null);
         f.add(quit);
+        f.add(CST);
+
+        f.setVisible(true);
     }
 
     void CreateSimpleTask(){
+        // Components
         JFrame CSTFrame = new JFrame();
+        JLabel CSTTitle = new JLabel("CREATE SIMPLE TASK");
         JTextField CSTName = new JTextField();
         JTextField CSTDescript = new JTextField();
         JTextField CSTDuration = new JTextField();
         JButton CSTCreate = new JButton();
+
+        // Properties
+        CSTFrame.setSize(600,400);
+        CSTTitle.setSize(500,50);
+        CSTTitle.setFont(UniFont());
+        CSTTitle.setLocation(50, 50);
+
+        // Layout
+        CSTFrame.setVisible(true);
     }
 
     void CreateCompositeTask(){
@@ -44,10 +78,7 @@ public class GUIViewer {
     }
 
     public GUIViewer(){
-        UniFont = new Font("Lemon", Font.BOLD, 60);
         f = new JFrame();
-        f.setSize(800,800);
         this.mainFrame();
-        f.setVisible(true);
     }
 }
